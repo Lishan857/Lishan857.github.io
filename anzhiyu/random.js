147648ebae4a9062c5a3905d51630e0ea9d5686e
Lishan857 <1216591053@qq.com>
2025-04-07 20:09:54 +0800

    Site updated: 2025-04-07 20:09:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/30/hello-world/"];function toRandomPost(){
+var posts=["2025/03/30/hello-world/","2025/04/07/计算机40小时配套单词/","2025/04/04/计算机40小时入门笔记/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
