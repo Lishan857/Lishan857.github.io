2f08d457abc835db47b931b46954c74a980f4c69
Lishan857 <1216591053@qq.com>
2025-05-23 21:57:16 +0800

    Site updated: 2025-05-23 21:57:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/30/hello-world/","2025/04/07/计算机40小时配套单词/","2025/04/04/计算机40小时入门笔记/"];function toRandomPost(){
+var posts=["2025/03/30/hello-world/","2025/04/07/计算机40小时配套单词/","2025/04/04/计算机40小时入门笔记/","2025/05/23/CS50-笔记/","2025/05/23/CS50-课程代码案例/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
